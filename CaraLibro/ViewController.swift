d5844763017dc64152cada4aaf61b5c43debb74f
Pablo Zapata <romahhzg@gmail.com>
2022-06-23 17:57:50 -0400

    Initial Commit

---
+++ CaraLibro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CaraLibro
+//
+//  Created by user190977 on 6/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction private func tapToCloseKeyboard(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+}
+
+
+
