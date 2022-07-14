0f67b3bbb99d68d3eb08005c93c3d787cc71ffc7
romahhzg <98505061+romahhzg@users.noreply.github.com>
2022-07-14 13:09:20 -0400

    Add files via upload

---
+++ CaraLibro-main-2/CaraLibro/CaraLibro/Service.swift
@@ -0,0 +1,48 @@
+//
+//  Service.swift
+//  CaraLibro
+//
+//  Created by user190977 on 7/14/22.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+
+class Service {
+    
+    static func signUpUser(email: String, password: String, name: String, namelastname: String, onSuccess: @escaping () -> Void, onError: @escaping (_ error: Error?) -> Void) {
+        let auth = Auth.auth()
+        
+        auth.createUser(withEmail: email, password: password) { (authResult, error) in
+            if error != nil {
+                onError(error!)
+               
+                return
+            }
+            upLoadToDatabase(email: email, name: name, namelastname: namelastname, onSuccess: onSuccess)     // aqui agregamos dato
+        }
+    }
+    
+
+    static func upLoadToDatabase(email: String, name: String, namelastname: String, onSuccess: @escaping () -> Void) {    // aqui agregamos dato
+        let ref = Database.database().reference()
+        let uid = Auth.auth().currentUser?.uid
+        
+        // Dentro de este hijo vamos a guardar la info de los usuarios
+        ref.child("usuarios").child(uid!).setValue(["correo": email, "nombre": name, "apellidos": namelastname])    // aqui agregamos dato
+        onSuccess()
+    }
+    
+    static func createAlertController(title: String, message: String) -> UIAlertController {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }
+        alert.addAction(okAction)
+        
+        return alert
+    }
+    
+}
